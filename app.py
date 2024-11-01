2beff5cd390218f7dcd16cfdcbfec6a1c1f4172a
RidiculousBuffal <3322946829@qq.com>
2024-11-01 09:12:17 +0800

    add AISearch fix CreateTable error

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from fastapi import FastAPI, UploadFile,File, Depends,Form
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.datastructures import FormData
 
+from backend.Const.Prompt import Prompt
+from backend.connector.AzureAISearchConnector import SEARCH_FROM_AZURE
 from backend.llm.DefaultLLM import default_chat
+from backend.llm.agent.extractQuestion import ExtractQuestion
 from backend.models.DatabaseConnector import DBConnector, ExtendedDBConnector
 from backend.connector.getEngine import checkConnection, getEngine
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
@@ -34,26 +37,32 @@ def clean_table_name(filename: str) -> str:
     table_name = re.sub(r'\W+', '_', table_name)  # 替换非字母数字字符为下划线
     now = datetime.now()
     # 格式化时间为字符串
-    timestamp_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    timestamp_str = now.strftime('%Y-%m-%d%H%M%S')
     return table_name+timestamp_str
 @app.post("/checkDB")
 async def checkDB(payload: DBConnector):
     return checkConnection(payload)
+
 
 @app.websocket("/chatWithSQL")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         data = await websocket.receive_json()
-        print(data)
         dbConnectorPayload = DBConnector(**data['dbConnector'])
-        sql_array = await getSQL(userQuery=data['userQuery'],dbconnector=dbConnectorPayload,ws=websocket)
+        extractedList = ExtractQuestion(userQuery=data['userQuery'], dbconnector=dbConnectorPayload)
+        knowledgeBaseChunk=[]
+        if data['AiSearch']:
+            for e in extractedList:
+                knowledgeBaseChunk = knowledgeBaseChunk + SEARCH_FROM_AZURE(e)
+        sql_array = await getSQL(userQuery=data['userQuery'],dbconnector=dbConnectorPayload,ws=websocket,extractedList=extractedList)
         payloads = []
         for sql in sql_array:
             tmpResult = execute_query(query=sql,dbConnector=dbConnectorPayload)
             await websocket.send_json(tmpResult.model_dump_json())
             payloads.append(tmpResult.payload)
-        stream = await default_chat(user_query=data['userQuery'],prompt=f"根据数据库查询内容回答问题:{str(payloads)}")
+        finalPrompt = Prompt.FinalPrompt(AiSearch=data['AiSearch'],SearchInKB=str(knowledgeBaseChunk),SearchInDataBase=str(payloads))
+        stream = await default_chat(user_query=data['userQuery'],prompt=finalPrompt)
         await websocket.send_json(Result(status=1,message='开始生成最终回答',payload='').model_dump_json())
         await DefaultLLMResponse(ws=websocket,stream=stream,message="最终回答")
         await websocket.close()
@@ -71,6 +80,7 @@ async def uploadTable(payload:ExtendedDBConnector=Form(...)):
             df = pd.read_excel(BytesIO(contents))
         table_name = clean_table_name(filename=payload.file.filename)
         engine = getEngine(payload)
+        print(df.head())
         try:
             for col in df.select_dtypes(include=['object']).columns:
                 df[col] = df[col].str.encode('utf-8').str.decode('utf-8')
